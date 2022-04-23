74deff9cea2482bc6e2cf507db3eadc424e0aa97
ornato-t <tommy.ornato@gmail.com>
2022-04-23 12:44:08 +0200

    Merging stuff, hope nothing breaks

---
+++ README.md
@@ -0,0 +1,2 @@
+# 5eFiller
+Filling D&amp;D character sheets using data pulled from the 5e.tools utility website
---
+++ index.js
@@ -0,0 +1,15 @@
+var pdfFiller = require('pdffiller');
+
+var sourcePDF = "docs/scheda.pdf";
+var destinationPDF = "docs/scheda_filled.pdf";
+var data = {
+    "0 1": "Fire Bolt",
+    "1 1": "Cure Wounds",
+    "2 4": "Heat Metal",
+    "3 2": "P A L L A  D I  F U O C O"
+};
+
+pdfFiller.fillForm(sourcePDF, destinationPDF, data, function(err) {
+    if (err) throw err;
+    console.log("In callback (we're done).");
+});
